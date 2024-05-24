1c42da3826ede55daa188e2d988ba1b8b39980e7
KennethNhunduru <133786329+KennethNhunduru@users.noreply.github.com>
2024-05-24 19:53:48 +0100

    Add project files.

---
+++ FinalAssignment.Website/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+//Functions to be called play and pause to change the background color and color of "Listen to this gem of a cover version!"
+
+function newColor() {
+    document.getElementById("favourite-song").style.color = "red";
+}
+function oldColor() {
+    document.getElementById("favourite-song").style.color = "black";
+}
